559231f0daf042eca1fbc7a4939b1af03165aad7
Jalal-Khan <jalalusafxai@gmail.com>
2022-03-11 16:49:18 +0500

    initial commit

---
+++ MySuperBank/BankAccount.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySuperBank
+{
+    public class BankAccount
+    {
+        public string number { get; }
+        public string Owner { get; set; }
+        public decimal Balance {
+            get
+            {
+                decimal balance = 0;
+                foreach (var item in allTransactions)
+                {
+                    balance = balance + item.Amount;
+                }
+                return balance;
+            }
+        }
+        private static int accountNumberSeed=1234567890;
+        private List<Transaction> allTransactions = new List<Transaction>();
+
+        public BankAccount(string name, decimal initialBalance)
+        {
+            this.Owner = name;
+            MakeDeposit(initialBalance, DateTime.Now, "initial Balance");
+            this.number = accountNumberSeed.ToString();
+            accountNumberSeed = accountNumberSeed + 1; 
+
+        }
+
+
+        public void MakeDeposit(decimal amount,DateTime date,string note)
+        {
+           
+            if(amount<=0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
+            }
+            var deposit = new Transaction(amount, date, note);
+            allTransactions.Add(deposit);
+
+        }
+
+        public void MakeWithdrawl(decimal amount,DateTime date,string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawl must be positive");
+            }
+            if (Balance - amount < 0)
+            {
+                throw new InvalidOperationException("Not sufficient funds for this withdrawl");
+            }
+            var withdrawl = new Transaction(-amount, date, note);
+            allTransactions.Add(withdrawl);
+        }
+
+        public string GetAccountHIstory()
+        {
+            var report = new StringBuilder();
+
+            //HEADER
+            report.AppendLine("Date\t\tAmount\tNote");
+            foreach(var item in allTransactions)
+            {
+                //ROWS
+                report.AppendLine($"{item.Date.ToShortDateString()}\t{item.Amount}\t{item.Notes} ");
+            }
+            return report.ToString();
+        }
+
+
+
+    }
+}
---
+++ MySuperBank/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace MySuperBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("siri", 1000);
+            //Console.WriteLine($"This is bank acount of serial number {account.number} is created for {account.Owner} having {account.Balance} Blance");
+
+            account.MakeWithdrawl(120,DateTime.Now,"charity");
+           // Console.WriteLine(account.Balance);
+
+            account.MakeWithdrawl(50, DateTime.Now, "Xbox");
+            //Console.WriteLine(account.Balance);
+
+            Console.WriteLine(account.GetAccountHIstory());
+
+
+
+
+
+
+
+
+
+            /*//comments
+            //Test for negaive balance
+            try
+            {
+                account.MakeWithdrawl(99999, DateTime.Now, "attemp to overdraw");
+            }
+            catch (InvalidOperationException e)
+            {
+                 Console.WriteLine("Exception caught trying to overdraw");
+                 Console.WriteLine(e.ToString());
+            }
+
+            //TESTING initial balance
+            try
+            {
+                var invalidAccount = new BankAccount("invalid", -55);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine("Exception caught creating account with negative balance");
+                Console.WriteLine(e.ToString());
+            }*/
+
+           
+
+            
+            
+        }
+    }
+}
